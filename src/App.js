ecf353c42ec43680cf024ede25364572b75903a0
Jakub Kunikowski <j.kunikowski.165@studms.ug.edu.pl>
2024-10-27 13:07:04 +0100

    app ready for deployment

--- src/App.js
+++ src/App.js
@@ -46,17 +46,36 @@ const clarifaiRequestOption = (imageUrl) => {
     return requestOptions;
   }
 
+const initialState = {
+  input: " ",
+  imageUrl:"",
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+  }
+}
+
 
 class App extends React.Component {
   constructor() {
     super()
-    this.state = {
-      input: " ",
-      imageUrl:"",
-      box: {},
-      route: 'signin',
-      isSignedIn: false
-    }
+    this.state = initialState;
+  }
+
+  loadUser = (data) => {
+    this.setState({ user: {
+            id: data.id,
+            name: data.name,
+            email: data.email,
+            entries: data.entries,
+            joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -85,6 +104,20 @@ class App extends React.Component {
 
     fetch("v2/models/" + MODEL_ID + "/outputs", clarifaiRequestOption(this.state.input))
     .then(response => {
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, {entries: count}))
+        })
+        .catch(console.log)
+      }
       response.json().then((res) =>  this.displayFaceBox(this.calculateFaceLocation(res)));
     })
     .catch(err => console.log("Bad response", err));
@@ -93,7 +126,7 @@ class App extends React.Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
@@ -113,7 +146,7 @@ class App extends React.Component {
       { this.state.route === 'home'
         ? <div>
             <Logo />
-            <Rank />
+            <Rank name={this.state.user.name} entries={this.state.user.entries}/>
             <ImageLinkForm 
             onInputChange={this.onInputChange} 
             onButtonSubmit={this.onButtonSubmit}/>
@@ -121,8 +154,8 @@ class App extends React.Component {
           </div>
       : (
         this.state.route === 'signin'
-        ? <Signin onRouteChange={this.onRouteChange}/>
-        : <Register onRouteChange={this.onRouteChange}/>
+        ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+        : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
         )
       }
       </div>
