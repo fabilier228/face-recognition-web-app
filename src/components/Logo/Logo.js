98f1220dbd5521e8d6b0c0011793191942b6359e
Jakub Kunikowski <j.kunikowski.165@studms.ug.edu.pl>
2024-10-27 13:06:40 +0100

    small update

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -8,7 +8,7 @@ const Logo = () => {
         return (
             <div className="ma4 mt0">
                 <Tilt className="Tilt br2 shadow-2 fill" options={{ max : 55 }} style={{ height: '120px', width: '120px' }} >
-                    <div className="Tilt-inner"><img style={{height: '120px', width: '120px'}} src={brain} className="box"/></div>
+                    <div className="Tilt-inner"><img style={{height: '120px', width: '120px'}} src={brain} className="box" alt=""/></div>
                 </Tilt>
             </div>
         )
